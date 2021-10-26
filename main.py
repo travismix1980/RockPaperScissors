3c8b047d2afb2438e78ccfb2542bb3a6108516c7
Travis A Mix <36829787+travismix1980@users.noreply.github.com>
2021-10-26 05:42:26 +0000

    the player and computer make their choices and the choices are printed to the console

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -25,3 +27,41 @@ scissors = '''
 ---.__(___)
 '''
 
+computer_options = ["r", "p", "s"]
+computer_choice = random.choice(computer_options)
+
+
+print("Welcome to Rock Paper Scissors\n")
+print("##############################\n\n")
+player_choice = input("Enter 'r' for rock, 'p' for paper and 's' for scissors: ")
+
+# set player choice to lower case
+player_choice = player_choice.lower()
+
+# spacing
+print("\n\n")
+
+# print player choice 
+if player_choice == "r":
+    print("Player chose rock!")
+    print(rock)
+elif player_choice == "p":
+    print("Player chose paper!")
+    print(paper)
+elif player_choice == "s":
+    print("Player chose scissors!")
+    print(scissors)
+# error 
+else:
+    print("Not a valid choice")
+
+# print computer choice
+if computer_choice == "r":
+    print("Computer chose rock!")
+    print(rock)
+elif computer_choice == "p":
+    print("Computer chose paper!")
+    print(paper)
+elif computer_choice == "s":
+    print("Computer chose scissors!")
+    print(scissors)
